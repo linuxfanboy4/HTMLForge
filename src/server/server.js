ba51fe6b39b863ddfe701a9771c97a86a59a479d
linuxfanboy4 <imlinuxfanboy@outlook.com>
2025-03-21 16:11:49 +0600

    Create server.js

---
+++ src/server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    
+    socket.on('joinRoom', (room) => {
+        socket.join(room);
+    });
+
+    socket.on('codeChange', (code) => {
+        socket.broadcast.emit('codeUpdate', code);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
